167f44dd4063e99e8a483095ae0c8a39d0209c6b
Jonas Otávio B. de Oliveira <120534373+Jonas01101111@users.noreply.github.com>
2024-08-05 14:15:39 -0300

    Update App.xaml.cs

--- MauiAppQQSUM/App.xaml.cs
+++ MauiAppQQSUM/App.xaml.cs
@@ -17,6 +17,234 @@ namespace MauiAppQQSUM
                     new Alternativa {Correta = false, Descricao = "Gasoso" },
                     new Alternativa {Correta = false, Descricao = "Vaporoso" }
                 }
+            },
+            new Pergunta
+            {
+                Id = 2,
+                Enunciado = "Qual é o personagem do folclore brasileiro que tem uma perna só?",
+                Alternativa = new()
+                {
+                    new Alternativa {Correta = false, Descricao = "CUCA" },
+                    new Alternativa {Correta = true, Descricao = "NEGRINHO DO PASTOREIO" },
+                    new Alternativa {Correta = false, Descricao = "BOITATÁ" },
+                    new Alternativa {Correta = false, Descricao = "SACI-PERERÊ" }
+                }
+            },
+            new Pergunta
+            {
+                Id = 3,
+                Enunciado = "Qual cantor é o pai da dupla Sandy e Júnior?",
+                Alternativa = new()
+                {
+                    new Alternativa {Correta = false, Descricao = "LEONARDO" },
+                    new Alternativa {Correta = true, Descricao = "XORORÓ" },
+                    new Alternativa {Correta = false, Descricao = "ZEZÉ DI CAMARGO" },
+                    new Alternativa {Correta = false, Descricao = "CHITÃOZINHO" }
+                }
+            },
+            new Pergunta
+            {
+                Id = 4,
+                Enunciado = "Qual é o nome dado ao estado da água em forma de gelo?",
+                Alternativa = new()
+                {
+                    new Alternativa {Correta = false, Descricao = "Líquido" },
+                    new Alternativa {Correta = true, Descricao = "Sólido" },
+                    new Alternativa {Correta = false, Descricao = "Gasoso" },
+                    new Alternativa {Correta = false, Descricao = "Vaporoso" }
+                }
+            },
+            new Pergunta
+            {
+                Id = 5,
+                Enunciado = "Qual é o nome dado ao estado da água em forma de gelo?",
+                Alternativa = new()
+                {
+                    new Alternativa {Correta = false, Descricao = "Líquido" },
+                    new Alternativa {Correta = true, Descricao = "Sólido" },
+                    new Alternativa {Correta = false, Descricao = "Gasoso" },
+                    new Alternativa {Correta = false, Descricao = "Vaporoso" }
+                }
+            },
+            new Pergunta
+            {
+                Id = 6,
+                Enunciado = "Qual é o nome dado ao estado da água em forma de gelo?",
+                Alternativa = new()
+                {
+                    new Alternativa {Correta = false, Descricao = "Líquido" },
+                    new Alternativa {Correta = true, Descricao = "Sólido" },
+                    new Alternativa {Correta = false, Descricao = "Gasoso" },
+                    new Alternativa {Correta = false, Descricao = "Vaporoso" }
+                }
+            },
+            new Pergunta
+            {
+                Id = 7,
+                Enunciado = "Qual é o nome dado ao estado da água em forma de gelo?",
+                Alternativa = new()
+                {
+                    new Alternativa {Correta = false, Descricao = "Líquido" },
+                    new Alternativa {Correta = true, Descricao = "Sólido" },
+                    new Alternativa {Correta = false, Descricao = "Gasoso" },
+                    new Alternativa {Correta = false, Descricao = "Vaporoso" }
+                }
+            },
+            new Pergunta
+            {
+                Id = 8,
+                Enunciado = "Qual é o nome dado ao estado da água em forma de gelo?",
+                Alternativa = new()
+                {
+                    new Alternativa {Correta = false, Descricao = "Líquido" },
+                    new Alternativa {Correta = true, Descricao = "Sólido" },
+                    new Alternativa {Correta = false, Descricao = "Gasoso" },
+                    new Alternativa {Correta = false, Descricao = "Vaporoso" }
+                }
+            },
+            new Pergunta
+            {
+                Id = 9,
+                Enunciado = "Qual é o nome dado ao estado da água em forma de gelo?",
+                Alternativa = new()
+                {
+                    new Alternativa {Correta = false, Descricao = "Líquido" },
+                    new Alternativa {Correta = true, Descricao = "Sólido" },
+                    new Alternativa {Correta = false, Descricao = "Gasoso" },
+                    new Alternativa {Correta = false, Descricao = "Vaporoso" }
+                }
+            },
+            new Pergunta
+            {
+                Id = 10,
+                Enunciado = "Qual é o nome dado ao estado da água em forma de gelo?",
+                Alternativa = new()
+                {
+                    new Alternativa {Correta = false, Descricao = "Líquido" },
+                    new Alternativa {Correta = true, Descricao = "Sólido" },
+                    new Alternativa {Correta = false, Descricao = "Gasoso" },
+                    new Alternativa {Correta = false, Descricao = "Vaporoso" }
+                }
+            },
+            new Pergunta
+            {
+                Id = 11,
+                Enunciado = "Qual é o nome dado ao estado da água em forma de gelo?",
+                Alternativa = new()
+                {
+                    new Alternativa {Correta = false, Descricao = "Líquido" },
+                    new Alternativa {Correta = true, Descricao = "Sólido" },
+                    new Alternativa {Correta = false, Descricao = "Gasoso" },
+                    new Alternativa {Correta = false, Descricao = "Vaporoso" }
+                }
+            },
+            new Pergunta
+            {
+                Id = 12,
+                Enunciado = "Qual é o nome dado ao estado da água em forma de gelo?",
+                Alternativa = new()
+                {
+                    new Alternativa {Correta = false, Descricao = "Líquido" },
+                    new Alternativa {Correta = true, Descricao = "Sólido" },
+                    new Alternativa {Correta = false, Descricao = "Gasoso" },
+                    new Alternativa {Correta = false, Descricao = "Vaporoso" }
+                }
+            },
+            new Pergunta
+            {
+                Id = 13,
+                Enunciado = "Qual é o nome dado ao estado da água em forma de gelo?",
+                Alternativa = new()
+                {
+                    new Alternativa {Correta = false, Descricao = "Líquido" },
+                    new Alternativa {Correta = true, Descricao = "Sólido" },
+                    new Alternativa {Correta = false, Descricao = "Gasoso" },
+                    new Alternativa {Correta = false, Descricao = "Vaporoso" }
+                }
+            },
+            new Pergunta
+            {
+                Id = 14,
+                Enunciado = "Qual é o nome dado ao estado da água em forma de gelo?",
+                Alternativa = new()
+                {
+                    new Alternativa {Correta = false, Descricao = "Líquido" },
+                    new Alternativa {Correta = true, Descricao = "Sólido" },
+                    new Alternativa {Correta = false, Descricao = "Gasoso" },
+                    new Alternativa {Correta = false, Descricao = "Vaporoso" }
+                }
+            },
+            new Pergunta
+            {
+                Id = 15,
+                Enunciado = "Qual é o nome dado ao estado da água em forma de gelo?",
+                Alternativa = new()
+                {
+                    new Alternativa {Correta = false, Descricao = "Líquido" },
+                    new Alternativa {Correta = true, Descricao = "Sólido" },
+                    new Alternativa {Correta = false, Descricao = "Gasoso" },
+                    new Alternativa {Correta = false, Descricao = "Vaporoso" }
+                }
+            },
+            new Pergunta
+            {
+                Id = 16,
+                Enunciado = "Qual é o nome dado ao estado da água em forma de gelo?",
+                Alternativa = new()
+                {
+                    new Alternativa {Correta = false, Descricao = "Líquido" },
+                    new Alternativa {Correta = true, Descricao = "Sólido" },
+                    new Alternativa {Correta = false, Descricao = "Gasoso" },
+                    new Alternativa {Correta = false, Descricao = "Vaporoso" }
+                }
+            },
+            new Pergunta
+            {
+                Id = 17,
+                Enunciado = "Qual é o nome dado ao estado da água em forma de gelo?",
+                Alternativa = new()
+                {
+                    new Alternativa {Correta = false, Descricao = "Líquido" },
+                    new Alternativa {Correta = true, Descricao = "Sólido" },
+                    new Alternativa {Correta = false, Descricao = "Gasoso" },
+                    new Alternativa {Correta = false, Descricao = "Vaporoso" }
+                }
+            },
+            new Pergunta
+            {
+                Id = 18,
+                Enunciado = "Qual é o nome dado ao estado da água em forma de gelo?",
+                Alternativa = new()
+                {
+                    new Alternativa {Correta = false, Descricao = "Líquido" },
+                    new Alternativa {Correta = true, Descricao = "Sólido" },
+                    new Alternativa {Correta = false, Descricao = "Gasoso" },
+                    new Alternativa {Correta = false, Descricao = "Vaporoso" }
+                }
+            },
+            new Pergunta
+            {
+                Id = 19,
+                Enunciado = "Qual é o nome dado ao estado da água em forma de gelo?",
+                Alternativa = new()
+                {
+                    new Alternativa {Correta = false, Descricao = "Líquido" },
+                    new Alternativa {Correta = true, Descricao = "Sólido" },
+                    new Alternativa {Correta = false, Descricao = "Gasoso" },
+                    new Alternativa {Correta = false, Descricao = "Vaporoso" }
+                }
+            },
+            new Pergunta
+            {
+                Id = 20,
+                Enunciado = "Qual é o nome dado ao estado da água em forma de gelo?",
+                Alternativa = new()
+                {
+                    new Alternativa {Correta = false, Descricao = "Líquido" },
+                    new Alternativa {Correta = true, Descricao = "Sólido" },
+                    new Alternativa {Correta = false, Descricao = "Gasoso" },
+                    new Alternativa {Correta = false, Descricao = "Vaporoso" }
+                }
             }
         };
         public App()
