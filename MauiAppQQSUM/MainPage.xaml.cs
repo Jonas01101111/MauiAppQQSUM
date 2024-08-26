628e6c2b0ce5f6126b87ebc65a2c939a75ffee88
Jonas01101111 <Usuario@CPD03-M06>
2024-08-26 14:36:53 -0300

    pq n funciona?

--- MauiAppQQSUM/MainPage.xaml.cs
+++ MauiAppQQSUM/MainPage.xaml.cs
@@ -2,7 +2,7 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        
 
         public MainPage()
         {
@@ -16,6 +16,42 @@
 
         private void Next_Clicked(object sender, EventArgs e)
         {
+            bool acertou = false;
+            string resp = "";
+            bool valor;
+
+            if (alt0.IsChecked)
+            {
+                if (bool) alt0.Value)
+                    {
+                    acertou = true;
+                    resp = alt0.Content.ToString();
+                    }
+            }
+            if (alt1.IsChecked)
+            {
+                if (bool) alt1.Value)
+                    {
+                    acertou = true;
+                    resp = alt1.Content.ToString();
+                }
+            }
+            if (alt2.IsChecked)
+            {
+                if (bool) alt2.Value)
+                    {
+                    acertou = true;
+                    resp = alt2.Content.ToString();
+                }
+            }
+            if (alt3.IsChecked)
+            {
+                if (bool) alt3.Value)
+                    {
+                    acertou = true;
+                    resp = alt3.Content.ToString();
+                }
+            }
 
         }
     }
